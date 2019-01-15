987244849397d05173545494ee7d49ce657dd0aa
Jared Harbison <jared.harbison@gmail.com>
2019-01-15 17:30:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,8 @@
 def line(array)
   if array.length == 0
     puts "The line is currently empty."
-  else array.each_with_index |
+  else array.each_with_index do |name, index|
+    
   end
 end
 # "The line is currently: 1. Logan 2. Avi 3. Spencer"
