cd78a5684cffed5fa27eb512314fe7423e6169df
Jared Harbison <jared.harbison@gmail.com>
2019-01-15 17:43:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(array, name)
 end
 
 def now_serving
-  if array.empty?
+  if array.empty? == true
     puts "There is nobody waiting to be served!"
   else 
     puts "Currently serving #{array.shift}."
