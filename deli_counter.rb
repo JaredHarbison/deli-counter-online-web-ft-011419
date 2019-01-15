4e6887b64b5860a138e5b9fa90aa544ffc9cfc39
Jared Harbison <jared.harbison@gmail.com>
2019-01-15 17:33:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,13 +3,12 @@ def line(array)
     puts "The line is currently empty."
   else 
     message = "The line is currently:"
-    array.each_with_index do |value, index| 
-    message += " #{index.to_i+1}. #{value}"
+    array.each_with_index do |name, index| 
+    message += " #{index.to_i+1}. #{name}"
   end 
   puts "#{message}"
   end
 end
-# "The line is currently: 1. Logan 2. Avi 3. Spencer"
 
 def take_a_number
   
